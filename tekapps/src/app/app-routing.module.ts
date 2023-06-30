f1bd4a7ee3b15b70a45a699965d1ab51b1bf2a30
12manel123 <manelcastellvicerezuela@gmail.com>
2023-07-01 00:21:35 +0200

    crear pagina register

--- tekapps/src/app/app-routing.module.ts
+++ tekapps/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   }
 ];
 @NgModule({
