baf8a9c6dfcef5b5554971914bf2aa104cc554cc
12manel123 <manelcastellvicerezuela@gmail.com>
2023-07-03 18:30:50 +0200

    tab1 mediohecho

--- tekapps/src/app/tab1/tab1.page.ts
+++ tekapps/src/app/tab1/tab1.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  directos = [
+    {
+      imagen: 'gameplay1.png',
+      titulo: 'Directo 1',
+      descripcion: 'Descripción del directo 1',
+      route:'juanjito'
+    },
+    {
+      imagen: 'gameplay2.png',
+      titulo: 'Directo 2',
+      descripcion: 'Descripción del directo 2',
+    }
+  ];
   constructor() {}
 
 }
