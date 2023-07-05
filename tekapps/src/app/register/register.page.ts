2e8b592318e8c9a352436f2a8a7fd296e957e1be
Alex Larrosa Vegas <a.larrouseandvegas@gmail.com>
2023-07-05 22:09:27 +0200

    Toy cansao

--- tekapps/src/app/register/register.page.ts
+++ tekapps/src/app/register/register.page.ts
@@ -17,17 +17,21 @@ export class RegisterPage {
     this.formularioRegister = new FormGroup({
       nombre: new FormControl('', Validators.minLength(1)),
       password: new FormControl('', Validators.minLength(1)),
-      NumeroDeTelefono: new FormControl('', Validators.minLength(1)),
-      FechaDeNacimiento: new FormControl('', Validators.minLength(1))
+      NumeroDeTelefono: new FormControl('', [Validators.minLength(1), Validators.pattern(/^[0-9]+$/)]),
+      FechaDeNacimiento: new FormControl('', Validators.required,)
+      
     });
+  }
+  aceptarTerminos(event: Event) {
+    event.stopPropagation();
+
+  }
+  mostrarTerminos() {
+    this.router.navigateByUrl('/terms');
   }
   onIngresar() {
     if (this.formularioRegister.valid) {
-      
-     
       this.router.navigateByUrl('/tabs/tab1');    } else {
-      
-
       alert("Debes de completar todos los campos");
     }
   }
