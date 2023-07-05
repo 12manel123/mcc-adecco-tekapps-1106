1939910670575dc7ecde5edd2a16928232165a0d
12manel123 <manelcastellvicerezuela@gmail.com>
2023-07-05 17:29:49 +0200

    darkmode hecho

--- tekapps/src/app/login/login.page.ts
+++ tekapps/src/app/login/login.page.ts
@@ -1,28 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { 
-FormGroup,
-FormControl,
-Validators,
-FormBuilder } from '@angular/forms';
+import { FormGroup,FormControl,Validators, } from '@angular/forms';
+import { Router, RouterFeature } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
   formularioLogin: FormGroup;
-  constructor(public fb: FormBuilder) {
 
-    
-    
-    this.formularioLogin =this.fb.group ({
-      'nombre': new FormControl("",Validators.required),
-      'password': new FormControl("",Validators.required)
-   
-    })
+  constructor(private router: Router) {
+    this.formularioLogin = new FormGroup({
+      nombre: new FormControl('*', Validators.required),
+      password: new FormControl('*', Validators.required)
+    });
   }
-  ngOnInit() {
+  onIngresar() {
+    if (this.formularioLogin.valid) {
+      // El formulario es válido, realiza la lógica necesaria para el inicio de sesión
+      console.log('Inicio de sesión exitoso');
+      this.router.navigateByUrl('/tabs/tab1');    } else {
+      // El formulario es inválido, muestra un mensaje de error o realiza alguna acción adicional
+      console.log('Error en el formulario de inicio de sesión');
+      alert("ERROR!");
+    }
   }
 
 
