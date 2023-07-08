7b0b39d9c586f446b28dc750028701f239e70803
Alex Larrosa Vegas <a.larrouseandvegas@gmail.com>
2023-07-08 20:27:43 +0200

    Soy furro y estoy cansaiddimo

--- tekapps/src/app/modulo-vista/canal/canal.page.ts
+++ tekapps/src/app/modulo-vista/canal/canal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-canal',
@@ -6,10 +7,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./canal.page.scss'],
 })
 export class CanalPage implements OnInit {
+    nombreCanal: string = "ElRubius";
+    imagen: string = "../assets/img/el rubius (2).png";
+    descripcion: string = "Bienvenido a mi canal. Aquí encontrarás contenido increíble.";
+  
+    images = [
+      {
+        title: "Jugando al Maincrah",
+        description: "Wtf no te vas a creer lo que pasó",
+        imageUrl: "/assets/img/rubius 1.png"
+      },
+      {
+        title: "24 h en directo",
+        description: "EXTENSIBLE!",
+        imageUrl: "/assets/img/rubius 2.png"
+      },
+      {
+        title: "Entrevista",
+        description: "Aquí con Auron.",
+        imageUrl: "/assets/img/rubius 3.png"
+      },
+      {
+        title: "Bettle Royale",
+        description: "Fornite (Torneo)",
+        imageUrl: "/assets/img/rubius 4.png"
+      },{
+        title: "Garrys mod",
+        description: "Garrys mod con amigos",
+        imageUrl: "/assets/img/rubius 5.png"
+      },
+      {
+        title: "Estaba detrás de tigre",
+        description: "Soy furro XD",
+        imageUrl: "/assets/img/rubius 6.png"
+      },
+      {
+        title: "Squid Games",
+        description: "Presentación",
+        imageUrl: "/assets/img/rubius 7.png"
+      },
+      {
+        title: "Entrevista",
+        description: "Jordi Wild y el Rubius",
+        imageUrl: "/assets/img/rubius 8.png"
+      },
+      {
+        title: "Charlando tranquilamente",
+        description: "E de explicaros una cosa",
+        imageUrl: "/assets/img/rubius 9.png"
+      },
+      {
+        title: "Juego de miedo ;)",
+        description: "Noche de terror",
+        imageUrl: "/assets/img/rubius 10.png"
+      },
+    ];
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
-
+  editarPerfil() {
+  }
 }
