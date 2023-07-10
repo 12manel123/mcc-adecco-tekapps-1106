a4c9a9d34726e634b78954c8dabdcec0ec015067
Jesus Arraras <jesus.arraras.jerez@gmail.com>
2023-07-10 18:20:11 +0200

    Tab2, medio Creado

--- tekapps/src/app/tab2/tab2.page.ts
+++ tekapps/src/app/tab2/tab2.page.ts
@@ -7,6 +7,51 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  directos = [
+    {
+      imagen: 'gameplay1.png',
+      titulo: 'Directo 1',
+      descripcion: 'Descripción del directo 1',
+      route:'juanjito'
+    },
+    {
+      imagen: 'gameplay2.png',
+      titulo: 'Directo 2',
+      descripcion: 'Descripción del directo 2',
+      route:'pepito'
+    }
+  ];
+  categorias = [
+    {
+      imagen: 'category1.jpg',
+      titulo: 'Just chating',
+      espectadores: 356030,
+      type:['IRL','Creative'],
+      route:'chating'
+    },
+    {
+      imagen: 'category2.jpg',
+      titulo: 'Eventos especiales',
+      espectadores: 1943,
+      type:['IRL'],
+      route:'event'
+    }
+  ];
+  grupos = [
+    {
+      imagen: 'group1.jpg',
+      route:'juegos'
+    },
+    {
+      imagen: 'group2.jpg',
+      route:'juegos'
+
+    },
+    {
+      imagen: 'group3.jpg',
+      route:'vireal'
+    }
+  ];
   constructor() {}
 
 }
