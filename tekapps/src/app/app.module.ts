3a3b02dabb93304a9fa7cd3e1453280415b050b1
12manel123 <manelcastellvicerezuela@gmail.com>
2023-07-21 17:37:13 +0200

    loguin firebase puesto

--- tekapps/src/app/app.module.ts
+++ tekapps/src/app/app.module.ts
@@ -7,26 +7,49 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 
+//import { AngularFirestoreModule } from '@angular/fire/compat/firestore'; 
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/compat/auth'
+import { AngularFireModule } from '@angular/fire/compat';
+const firebaseConfig = {
+  apiKey: "AIzaSyAtWMEi-cdPLM1eu3J0wFNwpgwZwuezzrc",
+  authDomain: "tekapps-c4324.firebaseapp.com",
+  databaseURL: "https://tekapps-c4324-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tekapps-c4324",
+  storageBucket: "tekapps-c4324.appspot.com",
+  messagingSenderId: "126998096037",
+  appId: "1:126998096037:web:08d7c57c124500c036454c",
+  measurementId: "G-RMN0FBND8P"
+};
+import { environment } from '../environments/environment';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
-import { AngularFireModule } from '@angular/fire/compat';
+
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(environment.firebase),HttpClientModule,TranslateModule.forRoot({
-    defaultLanguage: 'es', // Idioma predeterminado
-    loader: {
-      provide: TranslateLoader,
-      useFactory: HttpLoaderFactory,
-      deps: [HttpClient],
-    },
-  }),],
+  imports: [
+    BrowserModule, IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    //AngularFireModule.initializeApp(environment.firebase),
+    //AngularFirestoreModule,
+    AngularFireAuthModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'es', // Idioma predeterminado
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- tekapps/src/app/modulo-vista/login/login.module.ts
+++ tekapps/src/app/modulo-vista/login/login.module.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
-
+import { AuthService } from 'src/app/service/auth.service';
 @NgModule({
   imports: [
     CommonModule,
@@ -16,6 +16,7 @@ import { LoginPage } from './login.page';
     IonicModule,
     LoginPageRoutingModule
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage],
+  providers: [AuthService]
 })
 export class LoginPageModule {}
