11475bb703a23bd7f80a2aa339dfd48b6da2ed11
12manel123 <manelcastellvicerezuela@gmail.com>
2023-07-24 18:32:26 +0200

    Tab1 categoria hecho

--- tekapps/src/app/tab1/tab1.page.ts
+++ tekapps/src/app/tab1/tab1.page.ts
@@ -10,62 +10,42 @@ export class Tab1Page {
   directosJson: any[] = [];
   categoriasJson: any[] = [];
   gruposJson: any[] = [];
-  directosPorCategoria: { [categoria: string]: number } = {}; // Objeto para contar directos por categoría
+  categoriasOrdenadas: { categoria: string; conteo: number }[] = []; // Array para almacenar categorías ordenadas
 
- loadDataFromJson(): void {
+  loadDataFromJson(): void {
     this.http.get<any[]>('assets/channels.json').subscribe((data) => {
       this.directosJson = data.filter(item => item.isLive && item.direct);
       this.categoriasJson = this.directosJson.map(item => item.category).filter((value, index, self) => self.indexOf(value) === index);
       this.gruposJson = data.filter(item => !item.isLive);
-      this.directosPorCategoria = this.countDirectosPorCategoria();
+      this.categoriasOrdenadas = this.countDirectosPorCategoria();
+      this.categoriasOrdenadas.sort((a, b) => b.conteo - a.conteo); // Ordenar categorías por cantidad de directos (mayor a menor)
     });
   }
   constructor(private http: HttpClient) {}
-  countDirectosPorCategoria(): { [categoria: string]: number } {
+  countDirectosPorCategoria(): { categoria: string; conteo: number }[] {
     const conteo: { [categoria: string]: number } = {};
-    this.categoriasJson.forEach((directo) => {
-      if (!conteo[directo.category]) {
-        conteo[directo.category] = 1;
-      } else {
-        conteo[directo.category]++;
+    this.directosJson.forEach((directo) => {
+      if (directo.category) {
+        if (!conteo[directo.category]) {
+          conteo[directo.category] = 1;
+        } else {
+          conteo[directo.category]++;
+        }
       }
     });
-    return conteo;
+
+    // Convertir el objeto de conteo en un array de objetos {categoria: string, conteo: number}
+    const categoriasConConteo: { categoria: string; conteo: number }[] = [];
+    for (const categoria in conteo) {
+      categoriasConConteo.push({ categoria, conteo: conteo[categoria] });
+    }
+
+    return categoriasConConteo;
   }
 
   ngOnInit() {
     this.loadDataFromJson();
   }
-  directos = [//Esto es un array de todos los directos
-    {
-      imagen: 'gameplay1.png',
-      titulo: 'Directo 1',
-      descripcion: 'Descripción del directo 1',
-      route:'juanjito'
-    },
-    {
-      imagen: 'gameplay2.png',
-      titulo: 'Directo 2',
-      descripcion: 'Descripción del directo 2',
-      route:'pepito'
-    }
-  ];
-  categorias = [//Esto es un array de todas las categorias.
-    {
-      imagen: 'category1.jpg',
-      titulo: 'Just Chatting',
-      espectadores: 356030,
-      type:['IRL','Creative'],
-      route:'chating'
-    },
-    {
-      imagen: 'category2.jpg',
-      titulo: 'Eventos especiales',
-      espectadores: 1943,
-      type:['IRL'],
-      route:'event'
-    }
-  ];
   grupos = [//Esto es un array de todos los grupos.
     {
       imagen: 'group1.jpg',
