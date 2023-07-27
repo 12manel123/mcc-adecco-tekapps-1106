5b174dce0c6b297740fc0436be1fc40fb5e71182
12manel123 <manelcastellvicerezuela@gmail.com>
2023-07-27 18:05:33 +0200

    directos hecho

--- tekapps/src/app/modulo-vista/directos/directos.page.ts
+++ tekapps/src/app/modulo-vista/directos/directos.page.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';//Para que el ionic y el angular funcione bien.
-
+import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';//Para que el ionic y el angular funcione bien.
+import { IonContent, IonList } from '@ionic/angular';
 @Component({
   selector: 'app-directos',
   templateUrl: './directos.page.html',
   styleUrls: ['./directos.page.scss'],//Componentes de la pagina directos.
 })
-export class DirectosPage implements OnInit {
+export class DirectosPage implements OnInit, AfterViewChecked {
+  @ViewChild('contentRef', { static: false }) content!: IonContent;
   nombreCanal: string = '12Manel123';
   descripcion: string = '50000001 subscriptores';//Numero y nombre del canal.
   suscrito: boolean = false;
   notificaciones: boolean = false;
   myuser:string='Tekapps';
-  
+  @ViewChild('contentRef', { static: false }) contentRef!: IonContent;
+  @ViewChild('messageContainer', { read: ElementRef, static: false }) messageContainerRef!: ElementRef;
+  @ViewChild('messageList', { static: false }) messageList!: IonList;
+  // Resto de tu código
+
   mensajes: any[] = [
     { user:"User1",texto: 'Hola', propio: true },
     { user:"User2",texto: 'Eres lo mejor!', propio: false },
@@ -21,13 +26,37 @@ export class DirectosPage implements OnInit {
   ];//Funcionalidad del chat.
   showBackButton: boolean = false;
   nuevoMensaje: string = '';//Variables
-  
  
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewChecked() {
+    this.scrollToBottom();
   }
- 
+
+  ngOnInit() {
+   
+  }
+
+  onKeyPress(event: KeyboardEvent) {
+    if (event.keyCode === 13 && !event.shiftKey) {
+      event.preventDefault(); // Evita el comportamiento predeterminado del Enter (evitar salto de línea)
+      this.enviarMensaje();
+    }
+  }
+  enviarMensaje() {
+    if (this.nuevoMensaje.trim() !== '') {
+      const mensaje = { user: this.myuser, texto: this.nuevoMensaje, propio: true };
+      this.mensajes.push(mensaje);
+      this.nuevoMensaje = ''; // Limpiar el campo de entrada de mensajes después de enviarlo
+      console.log('Mensajes:', this.mensajes); // Agrega esta línea para verificar los mensajes
+    }
+  }
+    
+  
+  scrollToBottom() {
+    const messageContainer = this.messageContainerRef.nativeElement;
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+  }
   
   toggleBackButton() {
     this.showBackButton = !this.showBackButton;
@@ -53,13 +82,10 @@ export class DirectosPage implements OnInit {
     return this.suscrito ? 'Desuscribirse' : 'Subscribirse';//Decir si está subscrito o no.
   }
 
-  enviarMensaje() {
-    if (this.nuevoMensaje.trim() !== '') {
 
-      const mensaje = { user: this.myuser,texto: this.nuevoMensaje, propio: true };
-      this.mensajes.push(mensaje);
-      this.nuevoMensaje = '';//Envio del mensaje. Ventana.
-    }
+
+  
+
+ 
   }
   
-}
