0d856abfae5bcb2b0716d74ad810c60877addca9
12manel123 <manelcastellvicerezuela@gmail.com>
2023-09-01 17:50:04 +0200

    Algunos cambios de 8100

--- tekapps/src/app/modulo-vista/register/register.page.ts
+++ tekapps/src/app/modulo-vista/register/register.page.ts
@@ -1,25 +1,17 @@
 import { Component, OnInit } from '@angular/core';//es una interfaz que define un método que se ejecutará cuando el componente se inicialice.
-import { FormGroup,FormControl,Validators,FormBuilder } from '@angular/forms';//: Se importan clases y utilidades relacionadas con el manejo de formularios reactivos en Angular. Estas clases son utilizadas para crear y validar formularios en la aplicación.
-import { Router, RouterFeature } from '@angular/router';//es un servicio que se utiliza para manejar la navegación entre diferentes rutas en la aplicación.
+import { FormGroup,Validators,FormBuilder } from '@angular/forms';//: Se importan clases y utilidades relacionadas con el manejo de formularios reactivos en Angular. Estas clases son utilizadas para crear y validar formularios en la aplicación.
+import { Router,} from '@angular/router';//es un servicio que se utiliza para manejar la navegación entre diferentes rutas en la aplicación.
 import { HttpClient } from '@angular/common/http';//es un servicio que se utiliza para realizar peticiones HTTP, como obtener datos de una API o enviar datos a un servidor.
-import { Injectable } from "@angular/core";//se utiliza para decorar un servicio y permitir que pueda ser inyectado en otros componentes o servicios.
-import { Platform } from '@ionic/angular';// es un servicio que proporciona acceso a la ruta activa y a los parámetros de la URL.
-import { ActivatedRoute } from '@angular/router';//es un servicio que proporciona acceso a la ruta activa y a los parámetros de la URL.
-import { AlertController } from '@ionic/angular';//es un servicio que se utiliza para mostrar alertas y cuadros de diálogo en la interfaz de usuario.
+import { Platform,AlertController } from '@ionic/angular';// es un servicio que proporciona acceso a la ruta activa y a los parámetros de la URL.
 import { AuthService } from 'src/app/service/auth.service'; //Este servicio probablemente es propio de la aplicación y se utiliza para manejar la autenticación y autorización de usuarios.
-
-
-
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],//Componente del registro. 
-  
 })
 export class RegisterPage implements OnInit {
   usuario: FormGroup;//Export class de la pagina de registro.
   imagePreview!: string; // Agregar esta línea para la vista previa de la imagen
-
   constructor(public http: HttpClient, private router: Router,private auth:AuthService,private alertCtrl:AlertController ,private platform: Platform,private fb: FormBuilder) {
     //  Constructor de la pagina del registro.
     this.usuario= this.fb.group({
@@ -27,13 +19,11 @@ export class RegisterPage implements OnInit {
       email:['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       passwordConfirmation: ['', [Validators.required, Validators.minLength(6)]],
-
     });//Validadores de la pagina de registro.
   }
   onImageChange(event: Event) {
     const input = event.target as HTMLInputElement;
     const file = input.files?.[0];
-    
     if (file) {
       const reader = new FileReader();
       reader.onload = () => {
@@ -43,8 +33,7 @@ export class RegisterPage implements OnInit {
     }
   }
   
-  signin() {
-    // Verifica si las contraseñas coinciden
+  signin() {// Verifica si las contraseñas coinciden
     if (this.usuario.value.password === this.usuario.value.passwordConfirmation) {
       if(this.usuario.value.username.length>=3){
       this.auth.registerUser(this.usuario.value.email, this.usuario.value.password)
